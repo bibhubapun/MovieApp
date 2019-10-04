8f7413dc3e89279205866d216909c0e9ea75c607
bibhu.panda <bibhu.panda@cgi.com>
2019-10-04 09:54:56 +0530

    Lombok annotations added

--- src/main/java/com/stackroute/MovieApp/domain/Movie.java
+++ src/main/java/com/stackroute/MovieApp/domain/Movie.java
@@ -1,67 +1,24 @@
 package com.stackroute.MovieApp.domain;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Movie {
     @Id
     int id;
     String movieName;
     String plot;
     int releaseYear;
-
-    public Movie() {
-    }
-
-    public Movie(int id, String movieName, String plot, int releaseYear) {
-        this.id = id;
-        this.movieName = movieName;
-        this.plot = plot;
-        this.releaseYear = releaseYear;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getMovieName() {
-        return movieName;
-    }
-
-    public void setMovieName(String movieName) {
-        this.movieName = movieName;
-    }
-
-    public String getPlot() {
-        return plot;
-    }
-
-    public void setPlot(String plot) {
-        this.plot = plot;
-    }
-
-    public int getReleaseYear() {
-        return releaseYear;
-    }
-
-    public void setReleaseYear(int releaseYear) {
-        this.releaseYear = releaseYear;
-    }
-
-    @Override
-    public String toString() {
-        return "Movie{" +
-                "id=" + id +
-                ", movieName='" + movieName + '\'' +
-                ", plot='" + plot + '\'' +
-                ", releaseYear=" + releaseYear +
-                '}';
-    }
 }
